e8076cf8e624e3595b1101b0a12f7425b8dc43c0
samuel santhosh <samuel@arrivusystems.com>
2013-12-02 10:41:01 +0530

    send modules ad user objets to env

--- app/controllers/user_module_enrollments_controller.rb
+++ app/controllers/user_module_enrollments_controller.rb
@@ -1,9 +1,11 @@
 class UserModuleEnrollmentsController < ApplicationController
+  before_filter :require_user
   before_filter :require_context
   add_crumb(proc { t('#crumbs.permissions', "Permissions") }) { |c| c.send :named_context_url, c.instance_variable_get("@context"), :context_user_module_enrollments_url }
   before_filter { |c| c.active_tab = "permissions" }
   def index
-
+    js_env :COURSE_MODULES_FOR_ENROLLMENT => @context.context_modules.map(&:attributes).to_json
+    js_env :ENROLLED_COURSE_USERS => @context.students.map(&:attributes).to_json
   end
 
   def new
