5fe6f40b78d9d86c3e813b3b9e3a04372fbc4b98
SaravananNV <saran@arrivusystems.com>
2013-12-23 12:24:14 +0530

    seperate migration for allow_comments_on_wiki_page

--- db/migrate/20131019043658_add_type_to_wikipages.rb
+++ db/migrate/20131019043658_add_type_to_wikipages.rb
@@ -2,12 +2,10 @@ class AddTypeToWikipages < ActiveRecord::Migration
   tag :predeploy
   def self.up
     add_column :wiki_pages, :wiki_type, :string
-    add_column :wiki_pages, :allow_comments, :boolean
   end
 
   def self.down
     remove_column :wiki_pages, :wiki_type
-    remove_column :wiki_pages, :allow_comments
   end
 
 end
