248ca7f1908f9b87bc4a5aa1160a84b8d063b490
samuel santhosh <samuel@arrivusystems.com>
2014-01-29 18:43:35 +0530

    fix oauth accept screen

--- app/controllers/pseudonym_sessions_controller.rb
+++ app/controllers/pseudonym_sessions_controller.rb
@@ -629,7 +629,7 @@ class PseudonymSessionsController < ApplicationController
   def oauth2_confirm
     @provider = Canvas::Oauth::Provider.new(session[:oauth2][:client_id], session[:oauth2][:redirect_uri], session[:oauth2][:scopes])
     badge_host = URI.parse(session[:oauth2][:redirect_uri]).host
-    if (request.domain == badge_host.split('.')[1]) or (badge_host == "localhost")
+    if (request.domain.split('.')[0] == badge_host.split('.')[1]) or (badge_host == "localhost")
       redirect_to :controller =>  'pseudonym_sessions',:action => 'oauth2_accept'
     end
     if mobile_device?
