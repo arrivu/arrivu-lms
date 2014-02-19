85540f7e2040950fa5c59f9e2464e1b3046d395d
samuel santhosh <samuel@arrivusystems.com>
2014-02-19 15:30:50 +0530

    add yahoo and hotmail

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,6 @@ ActionController::Dispatcher.middleware.use OmniAuth::Builder  do
   provider :facebook, config[:facebook_key], config[:facebook_secret]
   provider :linkedin, config[:linkedin_key], config[:linkedin_secret]
   provider :google_oauth2, config[:google_oauth2_key], config[:google_oauth2_secret]
+  provider :yahoo, config[:yahoo_key], config[:yahoo_secret]
+  provider :windowslive, config[:windowslive_key], config[:windowslive_secret]
 end
