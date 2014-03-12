8e2cdb7a41f5bf9069df5f65d178897ad3c6c2e2
SaravananNV <saran@arrivusystems.com>
2014-03-12 14:48:00 +0530

    disucussion tag error fixed on group

--- app/models/group.rb
+++ app/models/group.rb
@@ -22,6 +22,7 @@ class Group < ActiveRecord::Base
   include CustomValidations
   include UserFollow::FollowedItem
 
+  acts_as_tagger
   attr_accessible :name, :context, :max_membership, :group_category, :join_level, :default_view, :description, :is_public, :avatar_attachment, :storage_quota_mb
   validates_presence_of :context_id, :context_type, :account_id, :root_account_id, :workflow_state
   validates_allowed_transitions :is_public, false => true
