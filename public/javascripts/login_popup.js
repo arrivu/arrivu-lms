9b706b2419b4a2021ff16bdf7eeaa9766170436e
samuel santhosh <samuel@arrivusystems.com>
2014-07-05 15:11:58 +0530

    fix popup error messages

--- public/javascripts/login_popup.js
+++ public/javascripts/login_popup.js
@@ -2,12 +2,13 @@ require([
     'i18n!accounts' /* I18n.t */,
     'jquery' /* $ */,
     'compiled/util/addPrivacyLinkToDialog',
+    'str/htmlEscape',
     'user_sortable_name',
     'jquery.instructure_forms' /* formSubmit */,
     'jqueryui/dialog',
     'compiled/jquery/fixDialogButtons' /* fix dialog formatting */,
     'compiled/jquery.rails_flash_notifications'
-], function(I18n, $, addPrivacyLinkToDialog) {
+], function(I18n, $, addPrivacyLinkToDialog,htmlEscape) {
 
     $("#log_in_popup").click(function(event) {
         event.preventDefault();
@@ -22,13 +23,19 @@ require([
     $("#e_learning_login_form").formSubmit({
         required: ['pseudonym_session[unique_id]','pseudonym_session[password]'],
         beforeSubmit: function(data) {
+            $(this).loadingImage();
             $("button").attr('disabled', true)
                 .filter(".submit_button").text(I18n.t('logging_message', "Logging In..."));
         },
         success: function(data) {
             $.flashMessage("Successfully Logged IN");
+            $(this).loadingImage('remove');
             window.location.href ="/"
 
+        },
+        error: function(data){
+            $(this).loadingImage('remove');
+            $("button").attr('disabled', false);
         }
     });
     $("#elearning_forgot_passowrd_link").click(function(event) {
@@ -42,5 +49,21 @@ require([
         $("#e_learning_forgot_password_form").hide();
         $(".ui-dialog-buttonset").find("#request_password_button").hide();
         $(".ui-dialog-buttonset").find("#elearning_login_button").show();
+    });
+
+    $("#e_learning_forgot_password_form").formSubmit({
+        object_name: 'pseudonym_session',
+        required: ['unique_id_forgot'],
+        beforeSubmit: function(data) {
+            $(this).loadingImage();
+        },
+        success: function(data) {
+            $(this).loadingImage('remove');
+            $.flashMessage(htmlEscape(I18n.t("password_confirmation_sent", "Password confirmation sent to %{email_address}. Make sure you check your spam box.", {email_address: $(this).find(".email_address").val()})));
+            $(".login_link:first").click();
+        },
+        error: function(data) {
+            $(this).loadingImage('remove');
+        }
     });
 });
