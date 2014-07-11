694ba835ca60f03a39fc4b2e5df433d22ad1634c
SaravananNV <saran@arrivusystems.com>
2014-07-11 15:19:37 +0530

    fix course pricings and disabling course images and description when course detail custom design disabled

--- vendor/plugins/e_learning/app/helpers/course_pricings_helper.rb
+++ vendor/plugins/e_learning/app/helpers/course_pricings_helper.rb
@@ -1,12 +1,11 @@
 module CoursePricingsHelper
-  def nooverlap?(course_ids,start_date,end_date)
-    course_ids.each do |course|
-      start_date1=Date.parse(course.start_at.to_s)
-      end_date1=Date.parse(course.end_at.to_s)
+  def nooverlap?(course_pricings,start_date,end_date)
+    course_pricings.each do |course_price|
+      start_date1=Date.parse(course_price.start_at.to_s)
+      end_date1=Date.parse(course_price.end_at.to_s)
       start_date=Date.parse(start_date.to_s)
       end_date=Date.parse(end_date.to_s)
-      account_id=course.account_id
-      if account_id==@domain_root_account.id
+      if start_date1 == start_date && end_date1 == end_date
         return unless((start_date1..end_date1).to_a & (start_date..end_date).to_a).empty?
       end
     end
